0e02c41a023ebf40c35d18fa274a24b6a114a5b2
hdcarr87 <hdmanley87@gmail.com>
2020-02-13 13:08:42 -0700

    mongo deploy

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
