2381981f1ec8bcebf8d44139baaa8ddfc6ab246f
hdcarr87 <hdmanley87@gmail.com>
2020-02-13 13:16:44 -0700

    deployed code line fixed

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
